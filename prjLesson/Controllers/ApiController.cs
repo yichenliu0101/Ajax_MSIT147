75d580950cf9db9a1d185f74263892daa8b24cee
yichenliu0101 <aaronliu890101@gmail.com>
2023-07-13 16:34:44 +0800

    Update

---
+++ prjLesson/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace prjLesson.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("Hello Content");
+            //return Content("<h2>Hello Content</h2>", "text/html");
+            return Content("<h2>Hello Content 好好好</h2>", "text/html", Encoding.UTF8);
+
+            //return Content("Index", "application/msword");
+        }
+    }
+}
---
+++ prjLesson/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace prjLesson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
